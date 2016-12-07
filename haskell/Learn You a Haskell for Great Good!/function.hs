87c077e66f61e16f9c11ef60d13cd9d749e56d6c
Eshin Kunishima <mikoim@users.noreply.github.com>
2016-12-07 17:51:34 +0900

    Update function.hs

--- haskell/Learn You a Haskell for Great Good!/function.hs
+++ haskell/Learn You a Haskell for Great Good!/function.hs
@@ -8,6 +8,7 @@ factorial :: Int -> Int
 factorial 0 = 1
 factorial n = n * factorial(n - 1)
 
+-- 'Num a =>' a is instance of Num class
 addVectors :: Num a => (a, a) -> (a, a) -> (a, a)
 addVectors (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
 
