4bddca7b5290a47a6ccab0edc2bc7e2a8163f739
Eshin Kunishima <ek@esh.ink>
2016-12-08 05:11:33 +0900

    remove function calls to load on ghci directly

--- haskell/Learn You a Haskell for Great Good!/type.hs
+++ haskell/Learn You a Haskell for Great Good!/type.hs
@@ -1,14 +1,17 @@
 removeNonHiragana :: [Char] -> [Char]
 removeNonHiragana s = [ c | c <- s, c `elem` ['あ'..'ん']]
 
-removeNonHiragana "巫女（みこ、ふじょ）は、主として日本の神に仕える女性のこと。" -- "みこふじょはとしてのにえるのこと"
+-- removeNonHiragana "巫女（みこ、ふじょ）は、主として日本の神に仕える女性のこと。" -- "みこふじょはとしてのにえるのこと"
 
 factorial :: Integer -> Integer
 factorial n = product [1..n]
+-- factorial 3 -- 6
 
 circumference :: Float -> Float
 circumference r = 2 * pi * r
+-- circumference 1 -- 6.2831855
 
 circumference' :: Double -> Double
 circumference' r = 2 * pi * r
+-- circumference' 1 -- 6.283185307179586
 
