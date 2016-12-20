b6e7c963e943689e587c6ab3752a8bbd11025fdd
Eshin Kunishima <ek@esh.ink>
2016-12-20 14:54:10 +0900

    add example of recursion

--- haskell/Learn You a Haskell for Great Good!/recursion.hs
+++ haskell/Learn You a Haskell for Great Good!/recursion.hs
@@ -12,3 +12,42 @@ replicate' n x
     | otherwise = x : replicate' (n-1) x
 
 -- replicate' 3 "lol" -- ["lol","lol","lol"]
+
+
+take' :: Int -> [a] -> [a]
+take' n _
+    | n <= 0 = []
+take' _ [] = []
+take' n (x:xs) = x : take' (n-1) xs
+
+-- take' 2 [1,2,3] -- [1,2]
+
+
+reverse' :: [a] -> [a]
+reverse' [] = []
+reverse' (x:xs) = reverse' xs ++ [x]
+
+-- reverse' [3,2,1]
+
+
+repeat' :: a -> [a]
+repeat' x = x : repeat' x
+
+-- take' 5 $ repeat' 1 -- [1,1,1,1,1]
+
+
+zip' :: [a] -> [b] -> [(a,b)]
+zip' _ [] = []
+zip' [] _ = []
+zip' (x:xs) (y:ys) = (x, y) : zip' xs ys
+
+-- zip' [1,2,3] [4,5,6] -- [(1,4),(2,5),(3,6)]
+
+
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' a [] = False
+elem' a (x:xs)
+    | a == x = True
+    | otherwise = elem' a xs
+
+-- elem' 2 [1,2,3] -- True
