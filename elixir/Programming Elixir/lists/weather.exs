dcb8b8223f71ef5edafeffa2acafaf4340fbb65f
Eshin Kunishima <ek@esh.ink>
2017-01-31 18:36:53 +0900

    weather.exs

---
+++ elixir/Programming Elixir/lists/weather.exs
@@ -0,0 +1,17 @@
+defmodule WeatherHistory do
+  def for_location_27([]), do: []
+  def for_location_27([[time, 27, temp, rain] | tail]) do
+    [[time, 27, temp, rain] | for_location_27(tail)]
+  end
+  def for_location_27([_ | tail]), do: for_location_27(tail)
+
+  def test_data do
+    [
+      [1366225622, 26, 15, 0.125],
+      [1366225622, 27, 15, 0.45],
+      [1366225622, 28, 21, 0.25]
+    ]
+  end
+end
+
+WeatherHistory.for_location_27 WeatherHistory.test_data
