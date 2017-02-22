9eaf0a25fefec649fd3246ba40edd7c590eed052
Eshin Kunishima <ek@esh.ink>
2017-02-23 00:02:47 +0900

    simplify ModulesAndFunctions-6

--- elixir/Programming Elixir/ModulesAndFunctions-6.exs
+++ elixir/Programming Elixir/ModulesAndFunctions-6.exs
@@ -1,5 +1,5 @@
 defmodule Chop do
-  def guess(actual, a..b), do: guess!(actual, a..b, div(b - a, 2) + a)
+  def guess(actual, a..b), do: guess!(actual, a..b, div(a + b, 2))
 
   defp guess!(actual, a..b, n) when actual == n do
     IO.puts "Is it #{n}"
@@ -8,12 +8,12 @@ defmodule Chop do
 
   defp guess!(actual, a..b, n) when n > actual do
     IO.puts "Is it #{n} | #{a} ~ #{b} >"
-    guess actual, a..n-1
+    guess actual, a..n - 1
   end
 
   defp guess!(actual, a..b, n) when n < actual do
     IO.puts "Is it #{n} | #{a} ~ #{b} <"
-    guess actual, n+1..b
+    guess actual, n + 1..b
   end
 end
 
