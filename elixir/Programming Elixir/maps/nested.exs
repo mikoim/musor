97b16781a6871c40c660473e24f1ce8628b7781c
Eshin Kunishima <ek@esh.ink>
2017-05-21 23:54:45 +0900

    nested.exs

---
+++ elixir/Programming Elixir/maps/nested.exs
@@ -0,0 +1,18 @@
+defmodule Customer do
+  defstruct name: "", company: ""
+end
+
+defmodule BugReport do
+  defstruct owner: %Customer{}, details: "", severity: 1
+end
+
+report = %BugReport{owner: %Customer{name: "Dave", company: "Pragmatic"}, details: "broken"}
+report = %BugReport{ report | owner: %Customer{ report.owner | company: "PragProg" }}
+put_in(report.owner.company, "PragProg")
+update_in(report.owner.name, &("Mr. " <> &1))
+
+nyan = %{nuko: %{su: "（*´・ω・）"}}
+get_in(nyan, [:nuko, :su])
+# "（*´・ω・）"
+get_and_update_in(nyan, [:nuko, :su], &{&1, &1 <> " ｽ"})
+# {"（*´・ω・）", %{nuko: %{su: "（*´・ω・） ｽ"}}}
